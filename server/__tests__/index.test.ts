40ca36273e1b98dd2e150307a3b10857154dc5d1
mateusvs <mateusviniciussilva07@gmail.com>
2022-10-19 01:26:22 -0300

    Feat: Adicionando Jest para testes automatizados

---
+++ server/__tests__/index.test.ts
@@ -0,0 +1,5 @@
+describe('testing jest instalation', () => {
+  test('simple calc', () => {
+    expect(2 * 2).toBe(4);
+  });
+});
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,5 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import routes from "./app/routes";
+import app from './app';
 import { db } from "./database/db";
-
-dotenv.config();
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(routes);
 
 app.listen(process.env.SERVER_PORT, async () => {
   await db.sync();
