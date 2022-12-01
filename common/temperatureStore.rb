1c6c7c946eff1cf393a6ce2dd7228a610ac3ff0a
unknown <wendellpguariento@gmail.com>
2022-11-30 21:05:18 -0300

    Chore: Melhorias script arduino

--- common/temperatureStore.rb
+++ common/temperatureStore.rb
@@ -1,6 +1,6 @@
 require "serialport"
 
-port_str = "COM7"
+port_str = "COM3"
 baud_rate = 9600
 data_bits = 8
 stop_bits = 2
@@ -9,10 +9,11 @@ parity = SerialPort::NONE
 sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
 
 loop do
+  puts sp.gets
   File.open('temperature.txt', 'w') do |out|
-    out.puts sp.getc
+    out.puts sp.gets
   end
-  puts sp.getc
+  puts sp.gets
 end
 
 sp.close
